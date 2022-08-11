74c05a26a6ce0ef6229fc2d91863a5e2521405fd
Marcin_Cer <midnajt0@gmail.com>
2022-08-11 14:49:17 +0200

    Learning the C# Syntax

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace GradeBook
+﻿using System.Collections.Generic;
+
+namespace GradeBook
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello, {args[0]}");
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 7.77 };
+            grades.Add(56.1);
+
+            System.Console.WriteLine(grades.Count);
+
+            var result = 0.0;
+            foreach (double number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            System.Console.WriteLine($"The average grade is {result:N2}");
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
